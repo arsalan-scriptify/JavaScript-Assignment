516bc62978929396a176a07c3a9baba8e7aff051
Arsalan Ahmed <arsalanahmed431@gmail.com>
2025-12-18 19:31:00 +0500

    completed chap 1-5 assignments

--- Chap 1-5/app.js
+++ Chap 1-5/app.js
@@ -4,3 +4,59 @@ alert("Welcome to JS LAND \n Happy Coding");
 
 var username;
 var myName="ArsalanAhmed";
+var message="Hello World";
+alert(message);
+
+    var student=prompt("enter your name");
+    var studentage=prompt("enter your age");
+    alert(student);
+    alert(studentage);
+     var pizza="pizza \npizz \npiz \npi \np";
+     alert(pizza);
+
+     var email="arsalanahmed431@gmail.com";
+     alert("My email address is " + email);
+
+     var book="A smarter way to learn Javascript";
+     alert("I am trying to learn JavaScript from the book "+book);
+
+     document.getElementById("first").innerHTML="I can write html using javascript";
+
+     // chapter 3
+
+     var age="21";
+     alert(age);
+
+     var birthYear="2004";
+
+     document.getElementById("birth").innerHTML;
+
+
+
+    var visitorName=prompt("Enter your name: ");
+    var productTitle=prompt("Enter the product title ");
+    var quantity=prompt("Enter the quantity of products you want to purchase: ");
+
+    var num1=prompt("Enter first number: ");
+    var num2=prompt("Enter second number: ");
+
+    var sum= Number (num1) + Number(num2);
+    var sum= Number (num1) - Number(num2);
+    var sum= Number (num1) / Number(num2);
+
+    var sum= Number (num1) * Number(num2);
+    alert(sum);
+
+    var value;
+    console.log(value);
+
+    value=5;
+    console.log("initial value is : "+value);
+    value++;
+    console.log("value after increment is: " +value);
+    value+=7;
+    console.log("after addition is: "+value);
+
+    value--;
+    console.log("value after decrement: "+value);
+     console.log("value after dividing: "+3/value);
