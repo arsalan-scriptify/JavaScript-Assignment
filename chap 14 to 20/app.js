b00ebf9a25ce0bc0a9dd2de6a86a5252d0651dc1
Arsalan Ahmed <arsalanahmed431@gmail.com>
2026-01-21 16:15:01 +0500

    completed assignment for chap 14 to 25

---
+++ chap 14 to 20/app.js
@@ -0,0 +1,58 @@
+// var stduentstatus=[];
+
+// var fruits=["mango","apple","banana","orange"];
+
+// var num=[2,5,6,21,66,88];
+
+// var bully=[true,false,true,false,true];
+
+// var mixarr=["Arsalan",23,true,"Karachi",95.2];
+
+
+// var names = ["arsalan", "ali", "ammar"];
+// var studentScores = [320, 230, 480];
+// var totalMarks = 500;
+
+// for (var i = 0; i < names.length; i++) {
+//     var percentage = (studentScores[i] / totalMarks) * 100;
+//     document.write("Score of " + names[i] + " is " + studentScores[i] + ". Percentage: " + percentage + "%<br>");
+// }
+
+// var mat = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ];
+
+// for(var i =1;i<=10;i++){
+//     console.log(i);
+// }
+
+// var table=Number(prompt("enter number: "));
+// var lenght=Number(prompt("enter length: "));
+
+// for(var i=1;i<=lenght;i++){
+//     console.log(" " + table, "x" ," " +i ,": " ,table*i);
+// }
+
+
+// var fruist=["mango","orange","apple","strawberry","berry"];
+
+//  for(var i=0;i<=fruist.length;i++){
+//     console.log(fruist[i]);
+//  }
+
+
+// for(var i=1;i<=15;i++){
+//     console.log(i);
+// }
+
+// for(var i=10;i>=1;i--){
+//     console.log(i);
+// }
+
+// for(var i=1;i<=19;i++){
+//     if(i%i!=0){
+//             console.log(i);
+//     }
+// }
---
+++ chap 21-30/app.js
@@ -0,0 +1,40 @@
+// var firstName=prompt("enter firstname: ");
+// var lastName=prompt("enter lastname: ");
+// var fullname=firstName+lastName;
+// console.log(fullname);
+
+
+// var mob=prompt("enter ur favorite mobile: ");
+// var totallength=mob.length;
+// console.log(totallength);
+
+// var country=["p","a","k","i","s","t","a","n"];
+
+// for(var i=0;i<=country.length;i++){
+//     if(country[i]=="n"){
+//         console.log("the index number of n is: "+i);
+//     }
+// }
+
+
+// var country=["p","a","k","i","s","t","a","n"];
+
+// for(var i=0;i<=country.length;i++){
+//     if(country[i]=="i"){
+//         console.log("the index number of n is: "+i);
+//     }
+// }
+
+
+// var country=["h","e","l","l","o","w","o","r","l","d"];
+
+// for(var i=0;i<=country.length;i++){
+//     if(country[i]=="l"){
+//         console.log("the index number of n is: "+i);
+//     }
+// }
+
+
+// var firstName=prompt("enter firstname: ");
+// var lastname=prompt("enter lastname: ");
+// console.log(firstName+lastname);
