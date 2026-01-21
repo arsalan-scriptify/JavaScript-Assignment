c150b558fca397f0212fcd5808e7d1133f27ea9d
Arsalan Ahmed <arsalanahmed431@gmail.com>
2026-01-21 15:20:38 +0500

    assignment chap 6to13

---
+++ Chap 6-13/app.js
@@ -0,0 +1,179 @@
+// var a=10;
+
+// console.log("the value of a is: "+a);
+
+// console.log("the value of ++a is: "+ ++a);
+
+// console.log("the value of a++ is: "+ a++);
+// console.log("the value of a is: "+a);
+// console.log("the value of --a is: "+ --a);
+// console.log("the value of a is: "+a);
+// console.log("the value of a-- is: "+ a--);
+// console.log("the value of a is: "+a);
+
+
+
+// var x=2;
+// var y=1;
+// var result=--x - --y + ++y + y--;
+// console.log(result);
+
+
+// var name=prompt("Enter your name: ");
+// alert("Greetings " +name);
+
+// var table=prompt("Enter the number you want table of: " ,"5");
+
+// for(var i=1;i<=10;i++){
+
+//     console.log(table*i);
+
+
+// }
+
+// var subject1=prompt("Enter first subject: ");
+// var subject2=prompt("Enter sec subject: ");
+// var subject3=prompt("Enter sec subject: ");
+// var mark=100;
+
+// var obmark1=Number(prompt("Enter the obtained marks: "));
+// var obmark2=Number(prompt("Enter the obtained marks: "));
+// var obmark3=Number(prompt("Enter the obtained marks: "));
+
+// var total1=mark/obmark1*100;
+// console.log(total1);
+
+//! Chapter 9
+
+// var cites=prompt("Enter the city name: ");
+
+// if(cites==="karachi"){
+//     console.log("Welcome to Karachi");
+// }
+// else{
+//     console.log("welcome");
+// }
+
+
+
+// var gender=prompt("Enter ur gender: ");
+// if(gender=="male"){
+//     console.log("welcome mister");
+// }
+// else if(gender=="female"){
+//     console.log("welcome miss");
+// }
+// else{
+//     console.log("Weclome");
+// }
+
+
+// var a=4;
+// if(++a==5){
+//     alert("given condition of variable a is true");
+// }
+// var b=82;
+// if(b++==83){
+//     alert("given condition of variable b is true");
+// }
+
+
+// var secret=5;
+// var guess=Number(prompt("Enter the numuber: "));
+// if(guess===secret){
+//             console.log("you won");
+// }
+
+// else if(guess>secret){
+//     console.log("your guess is higher");
+// }
+// else{
+//     console.log("your guess is lower");
+// }
+
+// var num=Number(prompt("enter number: "));
+// if(num%3==0){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// var evenorodd=Number(prompt("enter number: "));
+// if(evenorodd%2==0){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+
+// var firstNum=Number(prompt("enter number 1: "));
+// var secNum=Number(prompt("enter number 2: "));
+// var operation=prompt("Enter any operation: + - / * %");
+
+// if(operation==="+"){
+//     console.log(firstNum+secNum);
+// }
+// else if(operation==="-"){
+//     console.log(firstNum-secNum);
+// }
+// else if(operation==="/"){
+//     console.log(firstNum/secNum);
+// }
+// else if(operation==="*"){
+//     console.log(firstNum*secNum);
+// }
+// else if(operation==="%"){
+//     console.log(firstNum%secNum);
+// }
+
+
+
+// var str=prompt("enter any character: ");
+// if(typeof str==="number"){
+//     console.log("its a number");
+// }
+// else if(typeof str==="string"){
+//     console.log("its a string");
+// }
+
+// var num=Number(prompt("enter number: "));
+// var num1=Number(prompt("enter number: "));
+
+// if(num>num1){
+//     console.log("number1 is higher than number 2");
+// }
+// else if(num<num1){
+//     console.log("number 1 is lesser than number 2");
+// }
+// else{
+//     console.log("both numbers are equal");
+// }
+
+// var pass="ninja";
+// var check=prompt("enter ur password: ");
+// if(check===pass){
+//     console.log("welcome");
+// }
+// else{
+//     console.log("goodbye");
+// }
+
+// var time=prompt("enter time ex(1700): ");
+
+// if(time >="0000" && time <="1200"){
+// console.log("Good morning");
+// }
+
+// else if(time >="1200" && time <="1700"){
+// console.log("Good afternoon");
+// }
+
+// else if(time >="1700" && time <="2100"){
+// console.log("Good evening");
+// }
+
+// else if(time >="2100" && time <="2359"){
+// console.log("Good night");
+// }
